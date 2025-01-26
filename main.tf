04f885d4dd8d16a663b00daefd35a69a9f948ee8
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2025-01-26 14:50:35 +0300

    add bucket

--- main.tf
+++ main.tf
@@ -29,3 +29,23 @@ resource "yandex_iam_service_account" "sa_homework_2" {
   name        = "sa-homework-2"
   description = "service account for homework 2"
 }
+
+// Grant permissions
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = var.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa_homework_2.id}"
+}
+
+// Create Static Access Keys
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa_homework_2.id
+  description        = "static access key for object storage"
+}
+
+// Use keys to create bucket
+resource "yandex_storage_bucket" "bucket" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "vvot05-photo"
+}
